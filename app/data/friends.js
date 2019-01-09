a2ecd579d791cd67f7b289d4918b6edd0c12de03
sfattal <fattal.sherif@gmail.com>
2019-01-09 18:59:52 -0500

    debugging survey

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-[
+var usersArray = [
     {
         "name": "Ahmed",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
@@ -47,52 +47,6 @@
             "5"
         ]
     },
-    {
-        "name": "Louis T. Delia",
-        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-        "scores": [
-            "3",
-            "3",
-            "4",
-            "2",
-            "2",
-            "1",
-            "3",
-            "2",
-            "2",
-            "3"
-        ]
-    },
-    {
-        "name": "Lou Ritter",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
-        "scores": [
-            "4",
-            "3",
-            "4",
-            "1",
-            "5",
-            "2",
-            "5",
-            "3",
-            "1",
-            "4"
-        ]
-    },
-    {
-        "name": "Jordan Biason",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
-        "scores": [
-            "4",
-            "4",
-            "2",
-            "3",
-            "2",
-            "2",
-            "3",
-            "2",
-            "4",
-            "5"
-        ]
-    }
 ]
+
+module.exports = usersArray
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,11 +6,4 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     })
-
-    app.post("/api/friends", function(req, res) {
-        var newUser = req.body;
-
-        friends.push(newUser);
-        res.json(newUser);
-    });
 }
