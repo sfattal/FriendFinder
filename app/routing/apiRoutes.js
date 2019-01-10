5afe496b8e6c710666f3dc765a27325bd2763950
sfattal <fattal.sherif@gmail.com>
2019-01-09 19:01:12 -0500

    debugging survey

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,4 +6,11 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     })
+
+    app.post("/api/friends", function(req, res) {
+        var newUser = req.body;
+
+        friends.push(newUser);
+        res.json(newUser);
+    });
 }
