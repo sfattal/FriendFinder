1e779d26ee3651c5603ddd5ab549127b4fdde1ed
sfattal <fattal.sherif@gmail.com>
2019-01-11 15:24:07 -0500

    survey submission object output

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,5 @@
 var path = require('path');
-
+// var  getBestfriend = require()
 var friends = require("../data/friends.js")
 
 module.exports = function(app) {
@@ -9,8 +9,33 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res) {
         var newUser = req.body;
+        var userData = {
+            name: (req.body.full_name),
+            photo: (req.body.picture),
+            scores: [
+                parseInt(req.body.q_1),
+                parseInt(req.body.q_2),
+                parseInt(req.body.q_3),
+                parseInt(req.body.q_4),
+                parseInt(req.body.q_5),
+                parseInt(req.body.q_6),
+                parseInt(req.body.q_7),
+                parseInt(req.body.q_8),
+                parseInt(req.body.q_9),
+                parseInt(req.body.q_10)
+            ]
+        };
+        friends.push(userData);
+        var bestFriend = "Hey";
+        res.send(bestFriend);
+        console.log(res)
 
-        friends.push(newUser);
-        res.json(newUser);
+
+
+        // function match() {
+
+        // }
     });
+
+    
 }
--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 app.listen(PORT, function() {
   console.log('Friend Finder app is listening on PORT: ' + PORT);
